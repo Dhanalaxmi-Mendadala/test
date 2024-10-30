96cc912af342390bbf09ce845317b1bf43476ec5
kamakshi thapan <kamakshi.thapan@default.com>
2024-10-30 12:50:57 +0530

    |#1|Siddhu83/Sreenayak|fethced an API for the cover page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,19 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import WelcomePage from './welcomePage';
+import DashBoard from './DashBoard';
 
 function App() {
 
   return (
     <>
-    <Router>
-    <Routes>
-      <Route path='/' element={<WelcomePage />} />
-    </Routes>
-  </Router>
-
-  </>
+      <Router>
+        <Routes>
+          <Route path='/' element={<WelcomePage />} />
+          <Route path='/DashBoard' element={<DashBoard />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
