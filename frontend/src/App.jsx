23ab89f32d2d05a3a1ea77fe186ed1ee9bdc634b
nithindenkada <mohanraodenkada1@gmail.com>
2024-11-01 18:24:19 +0530

    |#19|Nithin/Revanth|Add a page to erote stories and add styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './homepage';
+import WelcomePage from './welcomePage';
 import WriteAStory from './writeANewStory';
-// import HomePage from './homepage';
-// import WelcomePage from './welcomePage';
 
 
 function App() {
@@ -10,9 +10,9 @@ function App() {
     <>
     <Router>
         <Routes>
-          <Route exact path='/' element={<WriteAStory />} />
-          {/* <Route path='/homepage' element={<HomePage/>}></Route>
-          <Route path='/homepage/addstory' element={<WriteAStory/>}/> */}
+          <Route exact path='/' element={<WelcomePage />} />
+          <Route path='/homepage' element={<HomePage/>}></Route>
+          <Route path='/homepage/WriteAStory' element={<WriteAStory/>}/>
         </Routes> 
       </Router>
     </>
--- frontend/src/saveDraft.jsx
+++ frontend/src/saveDraft.jsx
@@ -6,20 +6,20 @@ const saveDraft = (story, storyTitle) => {
     };
     console.log(object);
 } 
-async function putStory(story){
-    try {
-        const response = await fetch('http://localhost:8000/story',{
-          method: 'PUT',
-          headers: { 'Content-Type': 'json'},
-          mode:'cors',
-          credentials:"include",
-          body: story,
-        });
-        const data=await response.json();
-        console.log(data);
-        return data.isLoggedIn;
-      } catch{
-        return null;
-      }};
+// async function putStory(story){
+//     try {
+//         const response = await fetch('http://localhost:8000/story',{
+//           method: 'PUT',
+//           headers: { 'Content-Type': 'json'},
+//           mode:'cors',
+//           credentials:"include",
+//           body: story,
+//         });
+//         const data=await response.json();
+//         console.log(data);
+//         return data.isLoggedIn;
+//       } catch{
+//         return null;
+      // }};
 
 export default saveDraft;
