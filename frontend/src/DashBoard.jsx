8578bae0f698a2739ba8b7fc057b52f0b8124b1e
kamakshi thapan <kamakshi.thapan@default.com>
2024-11-02 20:01:27 +0530

    |#1|dhanalaxmi/siddhu|recieved the data from props and displayed the data

--- frontend/src/DashBoard.jsx
+++ frontend/src/DashBoard.jsx
@@ -3,26 +3,37 @@ import { useNavigate } from "react-router-dom"
 import PropTypes from 'prop-types'
 import './css/DashBoard.css'
 
+const generateTimeDifference = (timeStamp) => {
+  const present = new Date(timeStamp)
+  const month = present.getMonth() + 1;
+  const date = present.getDate();
+  return month + '-' + date;
+};
 
 const StoryComponent = ({ currentStory }) => {
   const [storyData, setStoryData] = useState({});
   const navigate = useNavigate();
   useEffect(() => {
     const fetchCoverPage = async () => {
-      const response = await fetch('http://localhost:8000/coverImage/cover-image.png');
 
+      const response = await fetch('http://localhost:8000/coverImage/cover-image.png');
       setStoryData(storyData => ({
         ...storyData,
-        image: response['url']
-      }))
-    }
-    fetchCoverPage();
+        image: response['url'],
+      }));
+    };
+    setStoryData(currentStory);
+    // setStoryData({ ...storyData, publishedTime: generateTimeDifference(storyData.published_at) })
+    fetchCoverPage(currentStory['']);
   }, []);
 
-  console.log(currentStory)
+  console.log(storyData, 'st story', currentStory);
+
+  const storyDescription = storyData['content'] ? storyData['content']['0']['data']['text'] : '';
+  console.log(storyDescription)
+
   return (
     <div className="story-component" onClick={() => {
-      console.log(storyData)
       navigate('/homepage/storypage', {
         state: {
           currentStory: storyData,
@@ -31,16 +42,18 @@ const StoryComponent = ({ currentStory }) => {
     }}>
 
       <div className="author-details">
-        <img src={'https://avatars3.githubusercontent.com/u/58026402?v=4'} alt="avatar" className="author-avatar" />
-        <h4 className="author-name">Naveen Kumar</h4>
+        <img src={`https://avatars3.githubusercontent.com/u/${storyData.authorId}?v=4`} alt="avatar" className="author-avatar" />
+        <h4 className="author-name">{storyData.author || 'Author'}</h4>
       </div>
       <div className="story-details">
-        <h3 className="title">Title</h3>
-        <p className="story-description"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, dolorum numquam ipsa quibusdam sed, dolores accusamus, perferendis voluptatem minima beatae rem aut nulla! Ea totam quasi deleniti. Ipsam, impedit facilis. Vev dolores quae natus quos, quisquam laborum sunt fugit. Culpa?</p>
+        <h3 className="title">{storyData.title || 'Title'}</h3>
+        <p className="story-description">
+          {storyDescription || 'Story decription'}
+        </p>
         <img src={storyData['image']} alt="cover-image" className="story-cover-image" />
       </div>
       <div className="story-meta-data">
-        <p className="published-time">Feb 30</p>
+        <p className="published-time">{storyData.publishedTime}</p>
         <p className="story-claps"></p>
         <p className="story-responses"></p>
       </div>
@@ -54,11 +67,12 @@ StoryComponent.propTypes = {
 
 
 const DashBoard = ({ stories }) => {
-  console.log(stories)
+  console.log('happy', stories)
   return (
-    <>
-      {stories.map((currentStory, i) => <StoryComponent key={i} currentStory={currentStory} />)}
-    </>
+    <div className="user-dashboard">
+      {stories ? stories.map((currentStory, i) => <StoryComponent key={i} currentStory={currentStory} />)
+        : <div className="error-message">Please follow Authors to see the stories</div>}
+    </div>
   )
 }
 
