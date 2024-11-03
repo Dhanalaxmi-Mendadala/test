f009bc29c10ff641ec8ad368806829392fc35e52
Dhanalaxmi-Mendadala <@default.com>
2024-11-03 18:12:50 +0530

    |#20|Dhanalaxmi|pull the data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './homepage';
 import WelcomePage from './welcomePage';
-import writeAStory from './writeANewStory';
 import StoryPage from './StoryPage.jsx';
 import WriteAStory from './writeANewStory';
 
