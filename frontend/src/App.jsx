a382219b8aacdfdb638865c96bb138668522b552
kamakshi thapan <kamakshi.thapan@default.com>
2024-11-03 11:49:11 +0530

    |#19|dhanalaxmi/siddhu|updated the darfting feature

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,19 +2,19 @@ import './css/App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import WelcomePage from './welcomePage';
 import HomePage from './homepage';
-import AddStory from './addstory.jsx';
 import StoryPage from './StoryPage.jsx';
+import WriteAStory from './writeANewStory.jsx';
 
 
 function App() {
   return (
     <>
-    <Router>
+      <Router>
         <Routes>
           <Route exact path='/' element={<WelcomePage />} />
-          <Route path='/homepage' element={<HomePage/>}></Route>
-          <Route path='/homepage/addstory' element={<AddStory/>}/>
-          <Route path='/homepage/storypage' element={<StoryPage/>}/>
+          <Route path='/homepage' element={<HomePage />}></Route>
+          <Route path='/homepage/addstory' element={<WriteAStory />} />
+          <Route path='/homepage/storypage' element={<StoryPage />} />
         </Routes>
       </Router>
     </>
--- frontend/src/addstory.jsx
+++
@@ -1,6 +0,0 @@
-function AddStory(){
-    return (<div>
-        <h1>Add a Story</h1>
-    </div>);
-}
-export default AddStory;
