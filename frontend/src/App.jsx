b61d6af9ee8e6236e91f6d7811d4a14c1ae4cc8b
Sreenayak <sreenayak711@gmail.com>
2024-11-04 10:32:57 +0530

    |#1|sreenayak|pull the data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,21 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import WelcomePage from './welcomePage';
+import Header from './Header';
+import DashBorad from './DashBoard';
 
 function App() {
 
   return (
     <>
-    <Router>
-    <Routes>
-      <Route path='/' element={<WelcomePage />} />
-    </Routes>
-  </Router>
-
-  </>
+      {/* <Router>
+        <Routes>
+          <Route path='/' element={<WelcomePage />} />
+        </Routes>
+      </Router> */}
+      <Header />
+      <DashBorad />
+    </>
   );
 }
 
