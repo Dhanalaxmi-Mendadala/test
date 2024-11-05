61b0026d2d2b06fc595da97dd0c92dfa84a031c9
nithindenkada <mohanraodenkada1@gmail.com>
2024-11-05 10:08:48 +0530

    |#19|Nithin/Dhanalaxmi|Adds the automatic saving feature functionality

--- frontend/src/saveDraft.jsx
+++ frontend/src/saveDraft.jsx
@@ -1,30 +1,31 @@
 const saveDraft = (story, storyTitle) => {
   const storyContent = story.blocks.map(content => {
+    console.log(storyContent, storyTitle)
     return { type: content.type, data: content.data }
   })
 
-  const object = {
-    storyId: 1,
-    title: storyTitle,
-    content: storyContent,
-  };
-}
+//   const object = {
+//     storyId: 1,
+//     title: storyTitle,
+//     content: storyContent,
+//   };
+// }
 
-async function putStory(story) {
-  try {
-    const response = await fetch('http://localhost:8000/story', {
-      method: 'PUT',
-      headers: { 'Content-Type': 'json' },
-      mode: 'cors',
-      credentials: "include",
-      body: story,
-    });
-    const data = await response.json();
-    console.log(data);
-    return data;
-  } catch {
-    return null;
-  }
+// async function putStory(story) {
+//   try {
+//     const response = await fetch('http://localhost:8000/story', {
+//       method: 'PUT',
+//       headers: { 'Content-Type': 'json' },
+//       mode: 'cors',
+//       credentials: "include",
+//       body: story,
+//     });
+//     const data = await response.json();
+//     console.log(data);
+//     return data;
+//   } catch {
+//     return null;
+//   }
 };
 
 export default saveDraft;
