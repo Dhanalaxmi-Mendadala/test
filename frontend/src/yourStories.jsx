46d0ff5181e77271a5e61ed2503bd43d8864875c
nithindenkada <mohanraodenkada1@gmail.com>
2024-11-07 10:25:04 +0530

    |#18|Nithin|Pulling the code

--- frontend/src/yourStories.jsx
+++ frontend/src/yourStories.jsx
@@ -1,12 +1,10 @@
-import { useState, useEffect } from "react"
-import { useNavigate } from "react-router-dom"
+import { useState, useEffect } from "react";
 import "./css/yourStories.css"
-import getStories from "./getStoriesApi"
+import getStories from "./getStoriesApi";
 import PropTypes from 'prop-types'
-import { publishStory } from "./publishStory"
 
 function YourStories() {
-  const [stories, setStoriesData] = useState(false);
+  const [stories, setStories] = useState(false);
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(true);
   useEffect(() => {
@@ -15,13 +13,12 @@ function YourStories() {
       if (data === null) {
         setError(true);
       } else {
-        setStoriesData(data);
+        setStories(data);
         setLoading(false);
       }
     };
     getStoriesData();
   }, []);
-
   if (error) {
     return (<div className="error">Error!Page not found,Page not found</div>);
   }
@@ -35,69 +32,169 @@ function YourStories() {
 };
 
 function Drafts({ drafts }) {
-  const navigator = useNavigate();
-  useEffect(() => { }
-    , [drafts]);
-
-  return (<div className="drafts-container">
-    {drafts.length === 0 ? <p>No Drafts yet,please created</p> :
-      <div className='all-drafts-unit'>{
-        drafts.map((draft, i) =>
-          <div className="draft-unit" key={i} >
-            <p className="draft-title" onClick={() => {
-              navigator('/homepage/storypage', {
-                state: {
-                  currentStory: draft,
-                }
-              })
-            }}>{draft['title']}</p>
-            <button className="edit-draft-button" onClick={() =>
-              navigator("/homepage/addstory", {
-                state: {
-                  id: draft['id'],
-                  content: draft['content']
-                }
-              })}>EDIT</button>
-            <button className="publish-button" onClick={() => {
-              publishStory(draft['id'])
-                .then(navigator('/homepage/yourstories'))
-            }}>PUBLISH</button>
-          </div>
-        )
-      }
-      </div>}
-  </div >)
+  console.log(drafts)
+  return (
+    <div className="drafts-container">
+      {drafts.length === 0 ? <p>No Drafts yet,please created</p> :
+        <div className="all-drafts-unit">
+          {drafts.map((draft, i) =>
+            <div className="draft-unit" key={i}>
+              <p className="draft-title">{draft['title'] || 'no title provided'}</p>
+              <button className="edit-draft-button">Edit</button>
+            </div>
+          )
+          }
+        </div>}
+    </div>)
 }
+
 Drafts.propTypes = {
   drafts: PropTypes.array.isRequired
 }
 
 function Publish({ published }) {
-  const navigator = useNavigate();
-  return (<div className="published-container">
-    {published.length === 0 ? <p>No published yet,please pulish a story</p> :
-      <div className='all-published-unit' > {
-        published.map((aStory, i) =>
-          <div className="a-story-unit" key={i} onClick={() => {
-            navigator('/homepage/storypage', {
-              state: {
-                currentStory: aStory,
-              }
-            })
-          }}>
-            <p className="a-story-title">{aStory['title']}</p>
-          </div>
-        )
-      }
-      </div>}
-  </div >)
+  console.log(published)
+  return (
+    <div className="published-container">
+      {published.length === 0 ? <p>No published stories yet,please create</p> :
+        <div className="all-published-unit">
+          {published.map((singleStory, i) =>
+            <div className="singleStory-unit" key={i}>
+              <p className="singleStory-title">{singleStory['title'] || 'no title provided'}</p>
+            </div>
+          )
+          }
+        </div>}
+    </div>)
 }
+
 Publish.propTypes = {
   published: PropTypes.array.isRequired
 }
 
+
+
+const STORIES = {
+  "drafts": [
+    {
+      "id": 3,
+      "title": "9 Ways to Build Virality into your Product",
+      "content": [
+        {
+          "type": "paragraph",
+          "data": {
+            "text": "I am a computer science student with a passion for design and all things aesthetic. I am also a minimalist and love keeping things extremely simple. I thought it would be nice to document my current computer setup and share how I organize my digital workspace."
+          }
+        },
+        {
+          "type": "paragraph",
+          "data": {
+            "text": "am a computer science student with a passion for design and all things aesthetic. I am also a minimalist and love keeping things extremely simple. I thought it would be nice to document my current computer setup and share how I organize my digital workspace."
+          }
+        },
+        {
+          "type": "header",
+          "data": {
+            "text": "Workspace",
+            "level": 2
+          }
+        },
+        {
+          "type": "paragraph",
+          "data": {
+            "text": "am a computer science student with a passion for design and all things aesthetic. I am also a minimalist and love keeping things extremely simple. I thought it would be nice to document my current computer setup and share how I organize my digital workspace."
+          }
+        },
+        {
+          "type": "delimiter",
+          "data": {}
+        },
+        {
+          "type": "header",
+          "data": {
+            "text": "Workspace",
+            "level": 2
+          }
+        },
+        {
+          "type": "paragraph",
+          "data": {
+            "text": "am a computer science student with a passion for design and all things aesthetic. I am also a minimalist and love keeping things extremely simple. I thought it would be nice to document my current computer setup and share how I organize my digital workspace."
+          }
+        }
+      ],
+      "published_at": null,
+      "author": "abhilashkasula",
+      "author_id": 58025056,
+      "tags": []
+    }
+  ],
+  "published": [
+    {
+      "id": 1,
+      "title": "9 Ways to Build Virality into your Product",
+      "content": [
+        {
+          "type": "paragraph",
+          "data": {
+            "text": "I am a computer science student with a passion for design and all things aesthetic. I am also a minimalist and love keeping things extremely simple. I thought it would be nice to document my current computer setup and share how I organize my digital workspace."
+          }
+        },
+        {
+          "type": "paragraph",
+          "data": {
+            "text": "am a computer science student with a passion for design and all things aesthetic. I am also a minimalist and love keeping things extremely simple. I thought it would be nice to document my current computer setup and share how I organize my digital workspace."
+          }
+        },
+        {
+          "type": "header",
+          "data": {
+            "text": "Workspace",
+            "level": 2
+          }
+        },
+        {
+          "type": "paragraph",
+          "data": {
+            "text": "am a computer science student with a passion for design and all things aesthetic. I am also a minimalist and love keeping things extremely simple. I thought it would be nice to document my current computer setup and share how I organize my digital workspace."
+          }
+        },
+        {
+          "type": "delimiter",
+          "data": {}
+        },
+        {
+          "type": "header",
+          "data": {
+            "text": "Workspace",
+            "level": 2
+          }
+        },
+        {
+          "type": "paragraph",
+          "data": {
+            "text": "am a computer science student with a passion for design and all things aesthetic. I am also a minimalist and love keeping things extremely simple. I thought it would be nice to document my current computer setup and share how I organize my digital workspace."
+          }
+        }
+      ],
+      "published_at": "2020-07-22 20:13:19",
+      "author": "abhilashkasula",
+      "author_id": 58025056,
+      "tags": [
+        "technology",
+        "maths",
+        "science",
+        "thriller",
+        "sci-fi"
+      ]
+    }
+  ]
+};
+
+
 function Stories({ stories }) {
   const [currentPage, setCurrentPage] = useState('draft');
+  console.log(stories)
   return (
     <div id="yourStories">
       <h1>Your Stories</h1>
@@ -106,14 +203,15 @@ function Stories({ stories }) {
         <a className="publish" onClick={() => setCurrentPage("publish")}>Published</a>
       </nav>
       {
-        currentPage === "draft" && <Drafts drafts={stories.drafts} /> ||
-        currentPage === "publish" && <Publish published={stories.published} />
+        currentPage === "draft" && <Drafts drafts={STORIES.drafts} /> ||
+        currentPage === "publish" && <Publish published={STORIES.published} />
       }
     </div>
   );
 }
+
 Stories.propTypes = {
-  stories: PropTypes.object.isRequired
+  stories: PropTypes.array.isRequired
 }
 
 export default YourStories;
