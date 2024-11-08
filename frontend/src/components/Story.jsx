12c8210ed15dda0d969452e1b641e2a625cc0819
kamakshi thapan <kamakshi.thapan@default.com>
2024-11-08 20:59:34 +0530

    |#15|siddhu/dhanalaxmi|updated the story page and fetched cover page

--- frontend/src/components/Story.jsx
+++ frontend/src/components/Story.jsx
@@ -10,6 +10,7 @@ import Delimiter from "@editorjs/delimiter";
 import copyLink from '../utilites/copyLink';
 import image1 from "../components/svg/notclicked1.svg"
 import image2 from "../components/svg/clicked-clap.svg"
+import fetchCoverPage from '../API/fetchCoverPage';
 
 const StoryContent = (props) => {
   const editorContainer = useRef(null);
@@ -28,8 +29,8 @@ const StoryContent = (props) => {
           header: {
             class: Header,
             inlineToolbar: true,
-            config:{
-              level:1,
+            config: {
+              level: 1,
             }
           },
           paragraph: {
@@ -58,7 +59,7 @@ const StoryPage = () => {
   const { id } = useParams();
   const [flag, setFlag] = useState(false);
   const [error, setError] = useState(false);
-  const [storyData, setStoryData] = useState(false);
+  const [storyData, setStoryData] = useState({});
   useEffect(() => {
     const getStoryData = async () => {
       const data = await fetchStory(id);
@@ -68,7 +69,13 @@ const StoryPage = () => {
         setStoryData(data['story']);
       }
     };
-    getStoryData();
+    const addImage = async () => {
+      const imageUrl = await fetchCoverPage(storyData['cover_image_name']);
+      setStoryData((currentData) => { return ({ ...currentData, imageUrl: imageUrl }) })
+      console.log(storyData, 'added the image')
+    }
+    getStoryData()
+      .then(() => addImage());
   }, []);
   if (error) {
     return (
@@ -77,46 +84,47 @@ const StoryPage = () => {
       </h1>
     );
   }
+  console.log(storyData, storyData['cover_image_name'], storyData['imageUrl'], 'completed fetching story data')
 
   const flagFunction = () => {
-    // const state = !flag;
     setFlag(!flag)
-}
+  }
   return (
     <>
       {
         storyData ? <main>
           <h1 className='main-title'>{storyData.title || 'Title'}</h1>
           <div className='story-author-details-container'>
-           <div><img className='story-author-image' src={`https://avatars3.githubusercontent.com/u/${storyData.authorId}?v=4`}></img></div> 
+            <div><img className='story-author-image' src={storyData['avatar_url']}></img></div>
             <div className='story-author-account-info-container'>
               <p className='story-author-name'>{storyData.author}</p>
               <p className='story-author-published'>{storyData.publications || ''}</p>
             </div>
           </div>
           <div className='all-actions-container'>
-           <div className='claps-response-cotainer'>
-           <div className='claps-container'>
-              <img src = {flag ? image2 : image1} onClick={flagFunction}  style={{
-                width: '20px', 
-                height: '20px'
-              }}/>
-              <span className='claps-count'>12</span>
+            <div className='claps-response-cotainer' title='claps'>
+              <div className='claps-container'>
+                <img src={flag ? image2 : image1} onClick={flagFunction} style={{
+                  width: '20px',
+                  height: '20px'
+                }} />
+                <span className='claps-count'>{storyData['clapsCount']}</span>
+              </div>
+              <div className='response-container' title='Response'>
+                <p className='response' >Response </p>
+                <span className='response-count'>{storyData['responsesCount']}</span>
+              </div>
             </div>
-            <div className='response-container'>
-              <p className='response' title='Response'>Response</p>
-              <span className='response-count'>12</span>
+            <div className='all-links-container'>
+              <div className='copy-link-container'>
+                <p className='copy-link' onClick={() => copyLink()} title='Copy Link'>Copy Link</p>
+              </div>
             </div>
-           </div>
-           <div className='all-links-container'>
-            <div className='copy-link-container'>
-              <p className='copy-link' onClick={()=>copyLink()}title='Copy Link'>Copy Link</p>
-            </div>
-           </div>
-          </div>
-          <div className='story-coverpage-container'>
-            <img className='story-coverpage' src={storyData.image || '../assets/story.jpeg'}></img>
-          </div>
+          </div>{
+            storyData['imageUrl'] &&
+            <div className='story-coverpage-container'>
+              <img className='story-coverpage' src={storyData['imageUrl']} />
+            </div>}
           <div className='story-content-container'>
             <StoryContent contentData={storyData.content} className='story-content' />
           </div>
