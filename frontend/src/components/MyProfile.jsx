b98fbdc103be231d09c8061d9b6d1c35b6933249
desktop-7hl82a3 dell <desktop-7hl82a3.dell@default.com>
2024-11-08 15:10:13 +0530

    |#7|sreenaik/revanth|Updated the UI of User Profile

--- frontend/src/components/MyProfile.jsx
+++ frontend/src/components/MyProfile.jsx
@@ -19,43 +19,47 @@ const Profile = () => {
   console.log(userData, "profile page");
   return (
     <>
-    <div className="about-user">
-    <h2 className="profile-user-name">{userData["username"]}</h2>
-      
-    </div>
-    <div className="profile">
-      <div className="profile-header">
-        <img
-          src={userData["avatar_url"]}
-          alt="Profile"
-          className="user-avatar"
-          style={{ height: "100px", width: "100px" }}
-        />
-        <h2 className="user-name">{userData["username"]}</h2>
+      <div className="about-user">
+        <h2 className="profile-user-name">{userData["username"]}</h2>
+       
       </div>
-      {userData["followers"] && userData["following"] && (
-        <>
-          <div className="profile-stats">
-            <p className="followers">
-              Followers :{userData["followers"].length}
-            </p>
-            <p className="following">
-              Following :{userData["following"].length}
-            </p>
-          </div>
-          <div className="followers-list">
-            <div>
+      <div className="profile">
+        <div className="profile-header">
+          <img
+            src={userData["avatar_url"]}
+            alt="Profile"
+            className="user-avatar"
+            style={{ height: "100px", width: "100px" }}
+          />
+          <h2 className="user-name">{userData["username"]}</h2>
+          <p className="user-id"><b>ID:</b> {userData["id"]}</p>
+          {userData["followers"] && userData["following"] && (
+          <>
+            <div className="profile-stats">
+              <p id="active" className="followers">
+                Followers {userData["followers"].length}
+              </p>
+              <div className="profile-stats-line">
+
+              </div>
+              <p id="active" className="following">
+                Following {userData["following"].length}
+              </p>
+            </div>
+          </>
+        )}
+        <div className="followers-list">
+            <div className="follower-data">
               <img className="followers-list-profile" src="https://avatars3.githubusercontent.com/u/58025056?v=4"></img>
               <p className="followers-list-username">abhi</p>
             </div>
-            <div>
+            <div className="follower-data">
               <img className="followers-list-profile" src="https://avatars3.githubusercontent.com/u/58025056?v=4"></img>
-              <p className="followers-list-username">naveen</p>
+              <p className="followers-list-username">naveen kumar varma vadla</p>
             </div>
           </div>
-        </>
-      )}
-    </div>
+        </div>
+      </div>
     </>
   );
 };
