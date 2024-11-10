9ad59d1786d378535487c02751fb15829b461bf2
Dhanalaxmi-Mendadala <@default.com>
2024-11-10 14:18:23 +0530

    |#19|dhanalaxmi|refactored the ui

--- frontend/src/components/ResponseofStory.jsx
+++ frontend/src/components/ResponseofStory.jsx
@@ -1,9 +1,11 @@
 import PropTypes from "prop-types";
+import "../css/response.css"
 function ResponseofStory(props){
     return <div id="response-outer" onClick={()=>props.setopenResponse(false)}>
-        <div id="response">
-        <div ><h1>Responses</h1> <h5 onClick={()=>props.setopenResponse(false)}>&times;</h5></div>
+        <div id="response" onClick={(e)=>{e.stopPropagation()}}>
+        <h1>Responses</h1> <h1 onClick={()=>props.setopenResponse(false)}>&times;</h1>
         <div >Response</div>
+        <input type="text"/>
         </div>
     </div>
 }
