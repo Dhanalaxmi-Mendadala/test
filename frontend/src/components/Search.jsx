1ad1e1225b2bb71344e36df2121ae4e5dcbb0865
kamakshi thapan <kamakshi.thapan@default.com>
2024-11-12 12:58:45 +0530

    |#10|siddhu/dhanalaxmi|added the search component to the router

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+// import { useState } from "react";
 
 const Search = () => {
-  const [searchTerm, setSearchTerm] = useState("");
+  // const [searchTerm, setSearchTerm] = useState("");
   return <>search</>
 }
 export default Search
