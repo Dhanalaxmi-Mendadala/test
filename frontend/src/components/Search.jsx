75b4198f36f3cbca8adfff7d12022cd602ae338f
Dhanalaxmi-Mendadala <@default.com>
2024-11-18 17:25:01 +0530

    |#6|nithin/dhanalaxmi|pull the data

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -4,8 +4,37 @@ import searchData from "../API/search";
 import StoryCard from "./StoryCard";
 import '../css/search.css'
 import searchImage from '../components/svg/1.svg'
+import { NavLink } from "react-router-dom";
 
-const SearchInput = ({ setResults, searchTerm, setSearchTerm }) => {
+const SearchDiv = ({ results }) => {
+  const [currentData, setcurrentData] = useState('content');
+  return <div>
+    <nav className="searchDiv">
+      <NavLink className={(e) => {return e.isActive ? 'navLinkTrue' : 'navLinkFalse'}} onClick={(e) => {setcurrentData('content'), console.log(e)}}>Content</NavLink>
+      <NavLink className={(e) => {return e.isActive ? 'navLinkTrue' : 'navLinkFalse'}} onClick={(e) => {setcurrentData('content'), console.log(e)}}>Tag</NavLink>
+      <NavLink className={(e) => {return e.isActive ? 'navLinkTrue' : 'navLinkFalse'}} onClick={(e) => {setcurrentData('content'), console.log(e)}}>Author</NavLink></nav>
+    <div>
+      {
+        results[`${currentData}Based`].length !== 0 ? <div>{
+          results[`${currentData}Based`].map((storyData, i) =>
+            <StoryCard key={i}
+              storyData={storyData}
+              username={storyData['author']}
+              userAvatar={storyData['avatar_url']}
+              userId={storyData['authorId']} />
+          )}</div> :
+          <div className="no-results-data-main-container">
+            <div className="no-results-data-container">
+              <p>Make sure all words are spelled correctly.</p>
+              <p>Try different keywords.</p>
+              <p>Try more general keywords.</p>
+            </div>
+            </div>
+  }
+</div>
+</div>
+}
+    const SearchInput = ({setResults, searchTerm, setSearchTerm}) => {
   return (
     <div className="input-main-container">
       <div className="input-container">
@@ -19,57 +48,41 @@ const SearchInput = ({ setResults, searchTerm, setSearchTerm }) => {
           if (e.key === 'Enter') {
             const makeSearch = async () => {
               const data = await searchData(searchTerm);
-              setResults(data['contentBased']);
+              setResults(data);
             };
             makeSearch();
           }
         }} />
       </div>
     </div>
-  )
+    )
 }
-SearchInput.propTypes = {
-  setResults: PropTypes.func.isRequired,
-  setSearchTerm: PropTypes.func.isRequired,
-  searchTerm: PropTypes.string.isRequired,
+    SearchInput.propTypes = {
+      setResults: PropTypes.func.isRequired,
+    setSearchTerm: PropTypes.func.isRequired,
+    searchTerm: PropTypes.string.isRequired,
 }
 
-const SearchResults = ({ results, searchTerm }) => {
-  console.log(results)
+    const SearchResults = ({results, searchTerm}) => {
   if (searchTerm) return (
     <div className="search-results">
       <div className="default-search-output">
         <span className="results-for">Results for </span>
         <span className="content-searched"> {searchTerm}</span>
       </div>
-      {
-        results.length !== 0 ?
-          results.map((storyData, i) =>
-            <StoryCard key={i}
-              storyData={storyData}
-              username={storyData['author']}
-              userAvatar={storyData['avatar_url']}
-              userId={storyData['authorId']} />) :
-          <div className="no-results-data-main-container">
-            <div className="no-results-data-container">
-              <p>Make sure all words are spelled correctly.</p>
-              <p>Try different keywords.</p>
-              <p>Try more general keywords.</p>
-            </div>
-          </div>
-      }
+      <SearchDiv results={results} />
     </div>
-  )
+    )
 }
-SearchResults.propTypes = {
-  results: PropTypes.object.isRequired,
-  searchTerm: PropTypes.string.isRequired
+    SearchResults.propTypes = {
+      results: PropTypes.object.isRequired,
+    searchTerm: PropTypes.string.isRequired
 }
 
 const Search = () => {
   const [results, setResults] = useState(null);
-  const [searchTerm, setSearchTerm] = useState("");
-  return (
+    const [searchTerm, setSearchTerm] = useState("");
+    return (
     <>
       <SearchInput setResults={setResults} searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       {
@@ -78,4 +91,4 @@ const Search = () => {
     </>)
 }
 
-export default Search
+    export default Search
